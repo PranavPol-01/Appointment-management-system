30aa65e93e302534f76e833154105e5037f99505
Siddhant Sathe <sathesiddhant21@gmail.com>
2024-08-05 21:59:46 +0530

    added role,gender,outled_id,staff_id

--- backend/models/signupUser.js
+++ backend/models/signupUser.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const outlet = require('./outlets');
 
 const signupUserSchema = new mongoose.Schema({
-    name: {
+    staff_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        unique: true
+    },
+
+    staff_name: {
         type: String,
         required: true,
         unique: true
@@ -14,6 +21,20 @@ const signupUserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    gender:{
+        type: String,
+        required: true
+    },
+    outlet_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'outlet',
+        required: true,
+        unique: true
     },
     createdAt: {
         type: Date,
