86e985f4eef0c51030017a53634b9daa48d21c93
Pranavlovescode <pranavtitambe04@gmail.com>
2024-09-07 18:10:21 +0530

    Done OtpverifyPage.jsx

--- ams/src/Pages/Login.jsx
+++ ams/src/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 // import Navbar from './../Components/Navbar';
@@ -36,6 +36,12 @@ function Login() {
       setLoading(false);
     }
   };
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      localStorage.removeItem("OTP")
+    },120000)
+  })
 
   //   const handleGoogleLogin = async () => {
   //     // Google login implementation
--- ams/src/Pages/OtpverifyPage.jsx
+++ ams/src/Pages/OtpverifyPage.jsx
@@ -1,54 +1,72 @@
-import { Button } from "@/Components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   InputOTP,
   InputOTPGroup,
-  InputOTPSlot,
-  InputOTPSeparator,
-} from "@/Components/ui/input-otp";
+  InputOTPSlot, 
+} from "@/components/ui/input-otp";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
 
 function OtpverifyPage() {
   const [otp, setOtp] = useState("");
-  const [inputOtp,setInputOtp]=useState("");
-  const handleSubmitOTP = async() => {
+  const [inputOtp, setInputOtp] = useState("");
+  const handleSubmitOTP = async () => {
     if (inputOtp == otp) {
-      console.log("This is correct OTP");      
-      const response = await axios.post("http://localhost:5000/api/verify-otp",{otp:Number(inputOtp)},{
-        headers:{
-          'Content-Type':'application/json',
+      console.log("This is correct OTP");
+      const response = await axios.post("http://localhost:5000/api/verify-otp", { otp: Number(inputOtp) }, {
+        headers: {
+          'Content-Type': 'application/json',
         }
       })
       const data = response.data;
       console.log(data);
-      localStorage.setItem('token',data.token)
-      setTimeout(()=>{
-        localStorage.removeItem("OTP")
-      },120000)   
+      localStorage.setItem('token', data.token)
     }
     else alert("Invalid OTP");
   };
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     console.log(localStorage.getItem("OTP"));
     setOtp(localStorage.getItem('OTP'));
+    setTimeout(()=>{
+      localStorage.removeItem("OTP")
+    },120000)
   })
 
   return (
     <>
-      <InputOTP maxLength={6} value={inputOtp} onChange={(inputOtp) => setInputOtp(inputOtp)}>
-        <InputOTPGroup>
-          <InputOTPSlot index={0} />
-          <InputOTPSlot index={1} />
-          <InputOTPSlot index={2} />
-          {/* <InputOTPSeparator/> */}
-          <InputOTPSlot index={3} />
-          <InputOTPSlot index={4} />
-          <InputOTPSlot index={5} />
-        </InputOTPGroup>
-      </InputOTP>
-      <Button onClick={handleSubmitOTP}>Verify OTP</Button>
+      <div className="flex justify-center mx-auto m-48">
+        {/* <Button onClick={handleSubmitOTP}>Verify OTP</Button> */}
+        <Card className="w-full max-w-sm shadow-md">
+          <CardHeader>
+            <CardTitle className="text-2xl">OTP Verification</CardTitle>
+            <CardDescription>
+              Enter the OTP sent on your registered mobile number below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="mx-auto">
+              {/* <Label htmlFor="otp">OTP</Label> */}
+              <InputOTP maxLength={6} value={inputOtp} onChange={(inputOtp) => setInputOtp(inputOtp)}>
+                <InputOTPGroup>
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                  {/* <InputOTPSeparator/> */}
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full" onClick={handleSubmitOTP}>Verify OTP</Button>
+          </CardFooter>
+        </Card>
+      </div>
     </>
   );
 }
