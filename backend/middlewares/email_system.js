2ac398ff95095190b3e5d13442e90a33c11ee043
Siddhant Sathe <sathesiddhant21@gmail.com>
2024-09-28 18:51:40 +0530

    automated email system added succesfully

---
+++ backend/middlewares/email_system.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+console.log('EMAIL:', process.env.EMAIL);
+console.log('PASSWORD:', process.env.PASSWORD);
+
+async function sendEmail(to, html) {
+
+    if (!process.env.EMAIL || !process.env.PASSWORD) {
+        console.error('Error: EMAIL and PASSWORD environment variables must be set.');
+        return;
+      }
+      
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // should be false for port 587
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: `Siddhant <sid210103@gmail.com>`,
+    to: to,
+    subject: "Test Email",
+    html: html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log(`Message sent: ${info.messageId}`);
+  } catch (error) {
+    console.error(`Error sending email: ${error}`);
+  }
+}
+
+// Example usage
+sendEmail("2004.sapp@gmail.com", "<h1>Siddhant here</h1> <p>Just testing the email system</p>");
